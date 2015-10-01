e49a73cff670097c910d3d454694f10d70f27460
James Messinger <james.messinger@gmail.com>
2015-10-01 13:41:29 -0500

    tooling updates

--- karma.conf.js
+++ karma.conf.js
@@ -7,8 +7,8 @@ var baseConfig = {
   reporters: ['mocha'],
   files: [
     // Third-Party Libraries
-    'tests/bower_components/chai/chai.js',
-    'tests/bower_components/sinon-js/sinon.js',
+    'www/bower_components/chai/chai.js',
+    'www/bower_components/sinon-js/sinon.js',
 
     // Json Schema $Ref Parser
     'dist/ref-parser.min.js',
