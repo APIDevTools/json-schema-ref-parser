8d6db7b4456fe286118154262830175fed2753c0
James Messinger <james.messinger@gmail.com>
2015-12-31 12:35:40 -0600

    better error handling in tests

--- tests/fixtures/helper.js
+++ tests/fixtures/helper.js
@@ -21,9 +21,22 @@
   /**
    * Throws an error if called.
    */
-  helper.shouldNotGetCalled = function shouldNotGetCalled() {
-    throw new Error('This function should not have gotten called.');
-  };
+   helper.shouldNotGetCalled = function shouldNotGetCalled(done) {
+     var err = new Error('This function should not have gotten called.');
+     if (typeof done === 'function') {
+       return function(err2) {
+         if (err2 instanceof Error) {
+           done(err2);
+         }
+         else {
+           done(err);
+         }
+       }
+     }
+     else {
+       throw err;
+     }
+   };
 
   /**
    * Tests the {@link $RefParser.resolve} method,
@@ -73,7 +86,7 @@
 
           done();
         })
-        .catch(helper.shouldNotGetCalled);
+        .catch(helper.shouldNotGetCalled(done));
     }
   };
 
