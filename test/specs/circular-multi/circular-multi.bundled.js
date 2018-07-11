5ec1be1aa0236f92aaf306368500e455369af911
James Messinger <james.messinger@gmail.com>
2018-07-11 06:43:54 -0500

    Refactored tests from PR #68

--- test/specs/circular-multi/circular-multi.bundled.js
+++ test/specs/circular-multi/circular-multi.bundled.js
@@ -1,47 +1,47 @@
-helper.bundled.complexOrder = {
+helper.bundled.circularMulti = {
   $schema: 'http://json-schema.org/draft-07/schema#',
   properties: {
     actions: {
       type: 'object',
       properties: {
         affirmativeAction: {
+          $ref: '#/properties/actions/properties/prevAction'
+        },
+        negativeAction: {
+          $ref: '#/properties/actions/properties/prevAction'
+        },
+        prevAction: {
           type: 'object',
           properties: {
             $id: 'text_assets',
             oneOf: [
               {
-                $ref: '#/properties/actions/properties/affirmativeAction/properties/definitions/asset'
+                $ref: '#/properties/actions/properties/prevAction/properties/definitions/asset'
               },
               {
-                $ref: '#/properties/actions/properties/affirmativeAction/properties/definitions/asset'
+                $ref: '#/properties/actions/properties/prevAction/properties/definitions/asset'
               }
             ],
             definitions: {
               switchWrapper: {
                 type: 'object',
-                $ref: '#/properties/actions/properties/affirmativeAction/properties/definitions/switch'
+                $ref: '#/properties/actions/properties/prevAction/properties/definitions/switch'
               },
               asset: {
                 type: 'object',
                 $id: 'asset_action',
                 properties: {
                   label: {
-                    $ref: '#/properties/actions/properties/affirmativeAction/properties'
+                    $ref: '#/properties/actions/properties/prevAction/properties'
                   }
                 }
               },
               switch: {
                 type: 'array',
-                $ref: '#/properties/actions/properties/affirmativeAction/properties/definitions/asset'
+                $ref: '#/properties/actions/properties/prevAction/properties/definitions/asset'
               }
             }
           }
-        },
-        negativeAction: {
-          $ref: '#/properties/actions/properties/affirmativeAction'
-        },
-        prevAction: {
-          $ref: '#/properties/actions/properties/affirmativeAction'
         }
       }
     }
--- test/specs/circular-multi/circular-multi.spec.js
+++ test/specs/circular-multi/circular-multi.spec.js
@@ -1,14 +1,14 @@
-describe('$refs that are in the same file but also circular', function () {
+describe('multiple circular $refs at the same depth in the schema', function () {
   'use strict';
 
   it('should bundle successfully', function () {
     var parser = new $RefParser();
 
     return parser
-      .bundle(path.rel('specs/complexOrder/definitions/root.json'))
+      .bundle(path.rel('specs/circular-multi/definitions/root.json'))
       .then(function (schema) {
         expect(schema).to.deep.equal(parser.schema);
-        expect(schema).to.deep.equal(helper.bundled.complexOrder);
+        expect(schema).to.deep.equal(helper.bundled.circularMulti);
       });
   });
 });
