b04f774277407c1943099dee0181a1c257b51648
Randall Krauskopf <randall.krauskopf@gmail.com>
2019-01-04 12:36:29 -0600

    Clean up karma scripts.

--- karma.conf.js
+++ karma.conf.js
@@ -2,7 +2,6 @@
 // https://karma-runner.github.io/0.12/config/configuration-file.html
 "use strict";
 
-
 module.exports = function (karma) {
   var config = {
     frameworks: ["mocha", "chai", "host-environment"],
@@ -29,7 +28,6 @@ module.exports = function (karma) {
     ]
   };
 
-  exitIfDisabled();
   configureCodeCoverage(config);
   configureLocalBrowsers(config);
   configureSauceLabs(config);
@@ -39,31 +37,11 @@ module.exports = function (karma) {
 };
 
 /**
- * If this is a CI job, and Karma is not enabled, then exit.
- * (useful for CI jobs that are only testing Node.js, not web browsers)
- */
-function exitIfDisabled () {
-  var CI = process.env.CI === "true";
-  var KARMA = process.env.KARMA === "true";
-
-  if (CI && !KARMA) {
-    console.warn("Karma is not enabled");
-    process.exit();
-  }
-}
-
-/**
  * Configures the code-coverage reporter
  */
 function configureCodeCoverage (config) {
   if (process.argv.indexOf("--coverage") === -1) {
     console.warn("Code-coverage is not enabled");
-    return;
-  }
-  else if (process.env.SAUCE === "true") {
-    // Code coverage causes sporadic failures on SauceLabs
-    // https://github.com/karma-runner/karma-sauce-launcher/issues/95#issuecomment-255020888
-    console.warn("Code-coverage is disabled for SauceLabs");
     return;
   }
 
@@ -87,15 +65,28 @@ function configureCodeCoverage (config) {
  * Configures the browsers for the current platform
  */
 function configureLocalBrowsers (config) {
-  var isMac = /^darwin/.test(process.platform);
-  var isWindows = /^win/.test(process.platform);
-  var isLinux = !isMac && !isWindows;
+  let isWindows = /^win/.test(process.platform) || process.env.WINDOWS === "true";
+  let isMac = /^darwin/.test(process.platform);
+  let isLinux = !isMac && !isWindows;
+  let isCI = process.env.CI === "true";
 
-  if (isMac) {
+  if (isCI) {
+    if (isWindows) {
+      // IE and Edge aren't available in CI, so use SauceLabs
+      configureSauceLabs(config);
+    }
+    else if (isMac) {
+      config.browsers = ["FirefoxHeadless", "ChromeHeadless", "Safari"];
+    }
+    else if (isLinux) {
+      config.browsers = ["FirefoxHeadless", "ChromeHeadless"];
+    }
+  }
+  else if (isMac) {
     config.browsers = ["Firefox", "Chrome", "Safari"];
   }
   else if (isLinux) {
-    config.browsers = ["Firefox", "ChromeHeadless"];
+    config.browsers = ["Firefox", "Chrome"];
   }
   else if (isWindows) {
     config.browsers = ["Firefox", "Chrome", "IE", "Edge"];
@@ -107,69 +98,42 @@ function configureLocalBrowsers (config) {
  * https://github.com/karma-runner/karma-sauce-launcher
  */
 function configureSauceLabs (config) {
-  var SAUCE = process.env.SAUCE === "true";
-  var username = process.env.SAUCE_USERNAME;
-  var accessKey = process.env.SAUCE_ACCESS_KEY;
+  let username = process.env.SAUCE_USERNAME;
+  let accessKey = process.env.SAUCE_ACCESS_KEY;
 
-  if (!SAUCE || !username || !accessKey) {
-    console.warn("SauceLabs is not enabled");
-    return;
+  if (!username || !accessKey) {
+    throw new Error(`SAUCE_USERNAME and/or SAUCE_ACCESS_KEY is not set`);
   }
 
-  var project = require("./package.json");
-  var testName = project.name + " v" + project.version;
-  var build = testName + " Build #" + process.env.TRAVIS_JOB_NUMBER + " @ " + new Date();
+  let project = require("./package.json");
 
   config.sauceLabs = {
-    build: build,
-    testName: testName,
+    build: `${project.name} v${project.version} Build #${process.env.TRAVIS_JOB_NUMBER}`,
+    testName: `${project.name} v${project.version}`,
     tags: [project.name],
   };
 
   config.customLaunchers = {
-    SauceLabs_IE_11: {
+    IE_11: {
       base: "SauceLabs",
-      platform: "Windows 10",
+      platform: "Windows 7",
       browserName: "internet explorer"
     },
-    SauceLabs_IE_Edge: {
+    Edge: {
       base: "SauceLabs",
       platform: "Windows 10",
       browserName: "microsoftedge"
-    },
-
-    // TODO FIXME TODO FIXME TODO FIXME TODO FIXME TODO FIXME TODO FIXME TODO
-    // ======================================================================
-    // I've temporarily commented-out Safari on Mac because SauceLabs seems to
-    // be having problems with their Mac VMs right now.
-    // https://travis-ci.org/APIDevTools/json-schema-ref-parser/jobs/440110398#L1169-L1194
-    // ======================================================================
-    // SauceLabs_Safari_Latest: {
-    //   base: 'SauceLabs',
-    //   platform: 'macOS 10.13',
-    //   browserName: 'safari'
-    // },
-
-    SauceLabs_Chrome_Latest: {
-      base: "SauceLabs",
-      platform: "Windows 10",
-      browserName: "chrome"
-    },
-    SauceLabs_Firefox_Latest: {
-      base: "SauceLabs",
-      platform: "Windows 10",
-      browserName: "firefox"
     },
   };
 
   config.reporters.push("saucelabs");
   config.browsers = Object.keys(config.customLaunchers);
-  config.concurrency = 1;
+  // config.concurrency = 1;
   config.captureTimeout = 60000;
   config.browserDisconnectTolerance = 5,
   config.browserDisconnectTimeout = 60000;
   config.browserNoActivityTimeout = 60000;
-  // config.logLevel = 'debug';
+  // config.logLevel = "debug";
 
   // The following tests tend to fail on SauceLabs,
   // probably due to zero-byte files and special characters in the paths.
