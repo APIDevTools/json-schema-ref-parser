cb5fb2c280fce1388bbaadebc647b21e4fc46c9a
Randall Krauskopf <randall.krauskopf@gmail.com>
2019-01-07 08:00:36 -0600

    additional code coverage exceptions

--- karma.conf.js
+++ karma.conf.js
@@ -144,6 +144,7 @@ function configureSauceLabs (config) {
     "test/specs/empty/**/*.spec.js",
     "test/specs/invalid/**/*.spec.js",
     "test/specs/parsers/**/*.spec.js",
+    "test/specs/substrings/**/*.spec.js",
     "test/specs/yaml.spec.js"
   ];
 }
