04b31a3bf49481e04ca11bde4a50cd6d62281884
Jakub Rożek <jakub@stoplight.io>
2020-04-22 22:25:39 +0200

    Throw errors with more meaningful paths

---
+++ test/specs/error-source/error-source.spec.js
@@ -0,0 +1,90 @@
+"use strict";
+
+const chai = require("chai");
+const chaiSubset = require("chai-subset");
+chai.use(chaiSubset);
+const { expect } = chai;
+const $RefParser = require("../../..");
+const helper = require("../../utils/helper");
+const path = require("../../utils/path");
+const { InvalidPointerError, ResolverError, MissingPointerError } = require("../../../lib/util/errors");
+
+describe("Report correct error source and path for", () => {
+  it("schema with broken reference", async () => {
+    const parser = new $RefParser();
+    try {
+      await parser.dereference({ foo: { bar: { $ref: "I do not exist" }}}, { continueOnError: true });
+      helper.shouldNotGetCalled();
+    }
+    catch (err) {
+      expect(err.errors).to.containSubset([
+        {
+          name: ResolverError.name,
+          source: source => typeof source === "string",
+          path: ["foo", "bar"],
+          message: message => typeof message === "string",
+        },
+      ]);
+    }
+  });
+
+  it("schema with a local reference pointing at property with broken external reference", async () => {
+    const parser = new $RefParser();
+    try {
+      await parser.dereference(path.abs("specs/error-source/broken-external.json"), { continueOnError: true });
+      helper.shouldNotGetCalled();
+    }
+    catch (err) {
+      expect(err.errors).to.containSubset([
+        {
+          name: ResolverError.name,
+          source: path.abs("specs/error-source/broken-external.json"),
+          path: ["components", "schemas", "testSchema", "properties", "test"],
+          message: message => typeof message === "string",
+        },
+      ]);
+    }
+  });
+
+  it("schema with a missing local pointer and reference pointing at external file with broken external", async () => {
+    const parser = new $RefParser();
+    try {
+      await parser.dereference(path.abs("specs/error-source/invalid-external.json"), { continueOnError: true });
+      helper.shouldNotGetCalled();
+    }
+    catch (err) {
+      expect(err.errors).to.containSubset([
+        {
+          name: MissingPointerError.name,
+          source: path.abs("specs/error-source/invalid-external.json"),
+          path: ["foo", "bar"],
+          message: message => typeof message === "string",
+        },
+        {
+          name: ResolverError.name,
+          source: path.abs("specs/error-source/broken-external.json"),
+          path: ["components", "schemas", "testSchema", "properties", "test"],
+          message: message => typeof message === "string",
+        },
+      ]);
+    }
+  });
+
+  it("schema with an invalid pointer", async () => {
+    const parser = new $RefParser();
+    try {
+      await parser.dereference(path.abs("specs/error-source/invalid-pointer.json"), { continueOnError: true });
+      helper.shouldNotGetCalled();
+    }
+    catch (err) {
+      expect(err.errors).to.containSubset([
+        {
+          name: InvalidPointerError.name,
+          source: path.abs("specs/error-source/invalid-pointer.json"),
+          path: ["foo", "baz"],
+          message: message => typeof message === "string",
+        },
+      ]);
+    }
+  });
+});
