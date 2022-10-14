00332368afb8e6660aae44765aa0d73c141e2a57
Florin Cotfasa <florin.cotfasa@keyloop.com>
2022-10-14 17:13:12 +0300

    Fixed a bug introduced by https://github.com/APIDevTools/json-schema-ref-parser/commit/4e0123f146c81f15abb49bac00cd63834c131792
    Because of that external schema are not mapped correctly (re-crawl was not executed) and the $ref was pointing out to a wrong path:
    "#/paths/~1test~1add-test/post/requestBody/content/application~1json/schema/properties/test2/properties/test3/properties/test5/items/properties/test5A/properties/test6/items"
    instead of
    "$ref": "#/components/schemas/Common.Test3/properties/test5/items/properties/test5A/properties/test6/items".
    (deep-external test contains a main file, a shared file and expected result)

---
+++ test/specs/deep-external/deep.spec.js
@@ -0,0 +1,16 @@
+"use strict";
+
+const { expect } = require("chai");
+const $RefParser = require("../../..");
+const path = require("../../utils/path");
+const bundledSchema = require("./bundled");
+
+
+describe("Schema with deeply-nested external $refs", () => {
+  it("should bundle successfully", async () => {
+    let parser = new $RefParser();
+    const schema = await parser.bundle(path.abs("specs/deep-external/Main-spec.yaml"));
+    expect(schema).to.equal(parser.schema);
+    expect(schema).to.deep.equal(bundledSchema);
+  });
+});

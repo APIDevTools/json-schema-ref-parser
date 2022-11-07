APIDevTools/json-schema-ref-parser

commit 4e0123f146c81f15abb49bac00cd63834c131792

fix: do not re-crawl unless necessary

lib/bundle.js
@@ -135,7 +135,9 @@ function inventory$Ref ($refParent, $refKey, path, pathFromRoot, indirections, i
   });
 
   // Recursively crawl the resolved value
-  crawl(pointer.value, null, pointer.path, pathFromRoot, indirections + 1, inventory, $refs, options);
+  if (!existingEntry) {
+    crawl(pointer.value, null, pointer.path, pathFromRoot, indirections + 1, inventory, $refs, options);
+  }
 }
 
 /**

commit 00332368afb8e6660aae44765aa0d73c141e2a57

Fixed a bug introduced by https://github.com/APIDevTools/json-schema-ref-parser/commit/4e0123f146c81f15abb49bac00cd63834c131792
Because of that external schema are not mapped correctly (re-crawl was not executed) and the $ref was pointing out to a wrong path:
"#/paths/~1test~1add-test/post/requestBody/content/application~1json/schema/properties/test2/properties/test3/properties/test5/items/properties/test5A/properties/test6/items"
instead of
"$ref": "#/components/schemas/Common.Test3/properties/test5/items/properties/test5A/properties/test6/items".
(deep-external test contains a main file, a shared file and expected result)

lib/bundle.js
@@ -135,7 +135,7 @@ function inventory$Ref ($refParent, $refKey, path, pathFromRoot, indirections, i
   });
 
   // Recursively crawl the resolved value
-  if (!existingEntry) {
+  if (!existingEntry || external) {
     crawl(pointer.value, null, pointer.path, pathFromRoot, indirections + 1, inventory, $refs, options);
   }
 }

test/specs/deep-external/Main-spec.yaml
@@ -1,0 +1,26 @@
+info:
+  title: Main 
+paths:
+  /test/add-test:
+    post:
+      requestBody:
+        content:
+          application/json:
+            schema:
+              $ref: "#/components/schemas/Test1"
+      responses:
+        '204':
+          description: No Content
+
+components:
+  schemas:
+    Test1:
+      properties:
+        test2:
+          $ref: "#/components/schemas/Test2"
+    Test2:
+      properties:
+        test3:
+          $ref: '#/components/schemas/Common.Test3'
+    Common.Test3:
+      $ref: 'shared.yaml#/components/schemas/Test4'

test/specs/deep-external/Shared.yaml
@@ -1,0 +1,44 @@
+info:
+  title: Shared 
+          
+components:
+  schemas:
+          
+    Test4:
+      properties:
+        test5:
+          items:
+            $ref: '#/components/schemas/Test5'
+              
+    Test5:
+      properties:
+        test5A:
+          $ref: '#/components/schemas/Test5A'
+        test5B:
+          $ref: '#/components/schemas/Test5B'
+
+    Test5A:
+      properties:
+        test6:
+          items:
+            $ref: '#/components/schemas/Test6'
+            
+    Test5B:
+      properties:
+        test6:
+          items:
+            $ref: '#/components/schemas/Test6'
+          
+    Test6:
+      properties:
+        type:
+          $ref: '#/components/schemas/Test7Type'
+        value:
+          format: double
+          type: number
+
+    Test7Type:
+      enum:
+        - VALUE1
+        - VALUE2
+        - VALUE3

test/specs/deep-external/bundled.js
@@ -1,0 +1,86 @@
+"use strict";
+
+module.exports =
+{
+  info: {
+    title: "Main"
+  },
+  paths: {
+    "/test/add-test": {
+      post: {
+        requestBody: {
+          content: {
+            "application/json": {
+              schema: {
+                $ref: "#/components/schemas/Test1"
+              }
+            }
+          }
+        },
+        responses: {
+          204: {
+            description: "No Content",
+          }
+        }
+      }
+    }
+  },
+  components: {
+    schemas: {
+      "Common.Test3": {
+        properties: {
+          test5: {
+            items: {
+              properties: {
+                test5A: {
+                  properties: {
+                    test6: {
+                      items: {
+                        properties: {
+                          type: {
+                            enum: [
+                              "VALUE1",
+                              "VALUE2",
+                              "VALUE3"
+                            ]
+                          },
+                          value: {
+                            format: "double",
+                            type: "number"
+                          }
+                        }
+                      }
+                    }
+                  }
+                },
+                test5B: {
+                  properties: {
+                    test6: {
+                      items: {
+                        $ref: "#/components/schemas/Common.Test3/properties/test5/items/properties/test5A/properties/test6/items"
+                      }
+                    }
+                  }
+                }
+              }
+            }
+          }
+        }
+      },
+      Test1: {
+        properties: {
+          test2: {
+            $ref: "#/components/schemas/Test2"
+          }
+        }
+      },
+      Test2: {
+        properties: {
+          test3: {
+            $ref: "#/components/schemas/Common.Test3"
+          }
+        }
+      }
+    }
+  }
+};

test/specs/deep-external/deep.spec.js
@@ -1,0 +1,16 @@
+"use strict";
+
+const { expect } = require("chai");
+const $RefParser = require("../../..");
+const path = require("../../utils/path");
+const bundledSchema = require("./bundled");
+
+
+describe("Schema with deeply-nested external $refs", () => {
+  it("should bundle successfully", async () => {
+    let parser = new $RefParser();
+    const schema = await parser.bundle(path.abs("specs/deep-external/Main-spec.yaml"));
+    expect(schema).to.equal(parser.schema);
+    expect(schema).to.deep.equal(bundledSchema);
+  });
+});

test/specs/deep-internal/Full-spec.yaml
@@ -1,0 +1,67 @@
+info:
+  title: Main 
+paths:
+  /test/add-test:
+    post:
+      requestBody:
+        content:
+          application/json:
+            schema:
+              $ref: "#/components/schemas/Test1"
+      responses:
+        '204':
+          description: No Content
+
+components:
+  schemas:
+    Test1:
+      properties:
+        test2:
+          $ref: "#/components/schemas/Test2"
+      
+    Test2:
+      properties:
+        test3:
+          $ref: '#/components/schemas/Common.Test3'
+      
+    Common.Test3:
+      $ref: '#/components/schemas/Test4'
+    
+    Test4:
+      properties:
+        test5:
+          items:
+            $ref: '#/components/schemas/Test5'
+              
+    Test5:
+      properties:
+        test5A:
+          $ref: '#/components/schemas/Test5A'
+        test5B:
+          $ref: '#/components/schemas/Test5B'
+
+    Test5A:
+      properties:
+        test6:
+          items:
+            $ref: '#/components/schemas/Test6'
+            
+    Test5B:
+      properties:
+        test6:
+          items:
+            $ref: '#/components/schemas/Test6'
+          
+    Test6:
+      properties:
+        type:
+          $ref: '#/components/schemas/Test7Type'
+        value:
+          format: double
+          type: number
+
+    Test7Type:
+      enum:
+        - VALUE1
+        - VALUE2
+        - VALUE3

test/specs/deep-internal/bundled.js
@@ -1,0 +1,104 @@
+"use strict";
+
+module.exports =
+{
+  info: {
+    title: "Main"
+  },
+  paths: {
+    "/test/add-test": {
+      post: {
+        requestBody: {
+          content: {
+            "application/json": {
+              schema: {
+                $ref: "#/components/schemas/Test1"
+              }
+            }
+          }
+        },
+        responses: {
+          204: {
+            description: "No Content",
+          }
+        }
+      }
+    }
+  },
+  components: {
+    schemas: {
+      "Common.Test3": {
+        $ref: "#/components/schemas/Test4"
+      },
+      Test1: {
+        properties: {
+          test2: {
+            $ref: "#/components/schemas/Test2"
+          }
+        }
+      },
+      Test2: {
+        properties: {
+          test3: {
+            $ref: "#/components/schemas/Test4"
+          }
+        }
+      },
+      Test4: {
+        properties: {
+          test5: {
+            items: {
+              $ref: "#/components/schemas/Test5"
+            }
+          }
+        }
+      },
+      Test5: {
+        properties: {
+          test5A: {
+            $ref: "#/components/schemas/Test5A"
+          },
+          test5B: {
+            $ref: "#/components/schemas/Test5B"
+          }
+        }
+      },
+      Test5A: {
+        properties: {
+          test6: {
+            items: {
+              $ref: "#/components/schemas/Test6"
+            }
+          }
+        }
+      },
+      Test5B: {
+        properties: {
+          test6: {
+            items: {
+              $ref: "#/components/schemas/Test6"
+            }
+          }
+        }
+      },
+      Test6: {
+        properties: {
+          type: {
+            $ref: "#/components/schemas/Test7Type"
+          },
+          value: {
+            format: "double",
+            type: "number"
+          }
+        }
+      },
+      Test7Type: {
+        enum: [
+          "VALUE1",
+          "VALUE2",
+          "VALUE3"
+        ]
+      }
+    }
+  }
+};

test/specs/deep-internal/deep.spec.js
@@ -1,0 +1,15 @@
+"use strict";
+
+const { expect } = require("chai");
+const $RefParser = require("../../..");
+const path = require("../../utils/path");
+const bundledSchema = require("./bundled");
+
+describe("Schema with deeply-nested internal $refs", () => {
+  it("should bundle successfully", async () => {
+    let parser = new $RefParser();
+    const schema = await parser.bundle(path.abs("specs/deep-internal/Full-spec.yaml"));
+    expect(schema).to.equal(parser.schema);
+    expect(schema).to.deep.equal(bundledSchema);
+  });
+});