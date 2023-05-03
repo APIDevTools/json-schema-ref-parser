51665dec5edbd1cb556e33a3be62a253ce6b46df
Daniel A. White <daniel.white@stoplight.io>
2023-05-03 13:54:43 -0400

    fix(http): replace url based basic auth with header

--- test/specs/http.spec.js
+++ test/specs/http.spec.js
@@ -1,8 +1,12 @@
 "use strict";
 
 const { host } = require("@jsdevtools/host-environment");
-const { expect } = require("chai");
+const { expect, use } = require("chai");
+const chai = require("chai");
+const spies = require("chai-spies");
 const $RefParser = require("../../lib");
+
+use(spies);
 
 describe("HTTP options", () => {
   let windowOnError, testDone;
@@ -25,9 +29,13 @@ describe("HTTP options", () => {
       let parser = new $RefParser();
 
       let schema = await parser.parse("https://nghttp2.org/httpbin/headers", {
-        resolve: { http: { headers: {
-          accept: "application/json"
-        }}}
+        resolve: {
+          http: {
+            headers: {
+              accept: "application/json",
+            },
+          },
+        },
       });
 
       expect(schema.headers).to.have.property("Accept", "application/json");
@@ -39,12 +47,19 @@ describe("HTTP options", () => {
         let parser = new $RefParser();
 
         let schema = await parser.parse("https://nghttp2.org/httpbin/headers", {
-          resolve: { http: { headers: {
-            "my-custom-header": "hello, world"
-          }}}
+          resolve: {
+            http: {
+              headers: {
+                "my-custom-header": "hello, world",
+              },
+            },
+          },
         });
 
-        expect(schema.headers).to.have.property("My-Custom-Header", "hello, world");
+        expect(schema.headers).to.have.property(
+          "My-Custom-Header",
+          "hello, world"
+        );
       });
     }
   });
@@ -74,10 +89,14 @@ describe("HTTP options", () => {
     it("should not follow 6 redirects by default", async () => {
       try {
         let parser = new $RefParser();
-        let schema = await parser.parse("https://nghttp2.org/httpbin/redirect/6");
+        let schema = await parser.parse(
+          "https://nghttp2.org/httpbin/redirect/6"
+        );
 
         if (host.node) {
-          throw new Error("All 6 redirects were followed. That should NOT have happened!");
+          throw new Error(
+            "All 6 redirects were followed. That should NOT have happened!"
+          );
         }
         else {
           // Some web browsers will automatically follow redirects.
@@ -87,17 +106,19 @@ describe("HTTP options", () => {
       }
       catch (err) {
         expect(err).to.be.an.instanceOf(Error);
-        expect(err.message).to.contain("Error downloading https://nghttp2.org/httpbin/redirect/6");
+        expect(err.message).to.contain(
+          "Error downloading https://nghttp2.org/httpbin/redirect/6"
+        );
         if (host.node) {
           expect(err.message).to.equal(
             "Error downloading https://nghttp2.org/httpbin/redirect/6. \n" +
-            "Too many redirects: \n" +
-            "  https://nghttp2.org/httpbin/redirect/6 \n" +
-            "  https://nghttp2.org/httpbin/relative-redirect/5 \n" +
-            "  https://nghttp2.org/httpbin/relative-redirect/4 \n" +
-            "  https://nghttp2.org/httpbin/relative-redirect/3 \n" +
-            "  https://nghttp2.org/httpbin/relative-redirect/2 \n" +
-            "  https://nghttp2.org/httpbin/relative-redirect/1"
+              "Too many redirects: \n" +
+              "  https://nghttp2.org/httpbin/redirect/6 \n" +
+              "  https://nghttp2.org/httpbin/relative-redirect/5 \n" +
+              "  https://nghttp2.org/httpbin/relative-redirect/4 \n" +
+              "  https://nghttp2.org/httpbin/relative-redirect/3 \n" +
+              "  https://nghttp2.org/httpbin/relative-redirect/2 \n" +
+              "  https://nghttp2.org/httpbin/relative-redirect/1"
           );
         }
       }
@@ -106,9 +127,12 @@ describe("HTTP options", () => {
     it("should follow 10 redirects if http.redirects = 10", async () => {
       let parser = new $RefParser();
 
-      let schema = await parser.parse("https://nghttp2.org/httpbin/redirect/10", {
-        resolve: { http: { redirects: 10 }}
-      });
+      let schema = await parser.parse(
+        "https://nghttp2.org/httpbin/redirect/10",
+        {
+          resolve: { http: { redirects: 10 }},
+        }
+      );
 
       expect(schema.url).to.equal("https://nghttp2.org/httpbin/get");
     });
@@ -116,27 +140,34 @@ describe("HTTP options", () => {
     it("should not follow any redirects if http.redirects = 0", async () => {
       try {
         let parser = new $RefParser();
-        let schema = await parser.parse("https://nghttp2.org/httpbin/redirect/1", {
-          resolve: { http: { redirects: 0 }}
-        });
+        let schema = await parser.parse(
+          "https://nghttp2.org/httpbin/redirect/1",
+          {
+            resolve: { http: { redirects: 0 }},
+          }
+        );
 
         if (host.node) {
-          throw new Error("The redirect was followed. That should NOT have happened!");
+          throw new Error(
+            "The redirect was followed. That should NOT have happened!"
+          );
         }
         else {
-        // Some web browsers will automatically follow redirects.
-        // Nothing we can do about that.
+          // Some web browsers will automatically follow redirects.
+          // Nothing we can do about that.
           expect(schema.url).to.equal("https://nghttp2.org/httpbin/get");
         }
       }
       catch (err) {
         expect(err).to.be.an.instanceOf(Error);
-        expect(err.message).to.contain("Error downloading https://nghttp2.org/httpbin/redirect/1");
+        expect(err.message).to.contain(
+          "Error downloading https://nghttp2.org/httpbin/redirect/1"
+        );
         if (host.node) {
           expect(err.message).to.equal(
             "Error downloading https://nghttp2.org/httpbin/redirect/1 \n" +
-          "uri requested responds with a redirect, redirect mode is set to error: " +
-          "https://nghttp2.org/httpbin/redirect/1"
+              "uri requested responds with a redirect, redirect mode is set to error: " +
+              "https://nghttp2.org/httpbin/redirect/1"
           );
         }
       }
@@ -154,10 +185,12 @@ describe("HTTP options", () => {
 
       // Swagger.io has CORS enabled, with "Access-Control-Allow-Origin" set to a wildcard ("*").
       // This should work by-default.
-      let schema = await parser.parse("https://petstore.swagger.io/v2/swagger.json");
+      let schema = await parser.parse(
+        "https://petstore.swagger.io/v2/swagger.json"
+      );
 
       expect(schema).to.be.an("object");
-      expect(schema).not.to.be.empty;  // eslint-disable-line no-unused-expressions
+      expect(schema).not.to.be.empty; // eslint-disable-line no-unused-expressions
       expect(parser.schema).to.equal(schema);
     });
 
@@ -166,12 +199,15 @@ describe("HTTP options", () => {
 
       // Swagger.io has CORS enabled, with "Access-Control-Allow-Origin" set to a wildcard ("*").
       // So, withCredentials MUST be false (this is the default, but we're testing it explicitly here)
-      let schema = await parser.parse("https://petstore.swagger.io/v2/swagger.json", {
-        resolve: { http: { withCredentials: false }}
-      });
+      let schema = await parser.parse(
+        "https://petstore.swagger.io/v2/swagger.json",
+        {
+          resolve: { http: { withCredentials: false }},
+        }
+      );
 
       expect(schema).to.be.an("object");
-      expect(schema).not.to.be.empty;  // eslint-disable-line no-unused-expressions
+      expect(schema).not.to.be.empty; // eslint-disable-line no-unused-expressions
       expect(parser.schema).to.equal(schema);
     });
 
@@ -182,20 +218,51 @@ describe("HTTP options", () => {
 
           // Swagger.io has CORS enabled, with "Access-Control-Allow-Origin" set to a wildcard ("*").
           // So, withCredentials MUST be false (this is the default, but we're testing it explicitly here)
-          let schema = await parser.parse("https://petstore.swagger.io/v2/swagger.json", {
-            resolve: { http: { withCredentials: true }}
-          });
+          let schema = await parser.parse(
+            "https://petstore.swagger.io/v2/swagger.json",
+            {
+              resolve: { http: { withCredentials: true }},
+            }
+          );
 
           // The request succeeded, which means this browser doesn't support CORS.
           expect(schema).to.be.an("object");
-          expect(schema).not.to.be.empty;  // eslint-disable-line no-unused-expressions
+          expect(schema).not.to.be.empty; // eslint-disable-line no-unused-expressions
           expect(parser.schema).to.equal(schema);
         }
         catch (err) {
           // The request failed, which is expected
-          expect(err.message).to.contain("Error downloading https://petstore.swagger.io/v2/swagger.json");
+          expect(err.message).to.contain(
+            "Error downloading https://petstore.swagger.io/v2/swagger.json"
+          );
         }
       });
     }
+  });
+
+  describe("http.basicAuth", () => {
+    const sandbox = chai.spy.sandbox();
+
+    beforeEach(() => {
+      sandbox.on(host.global, ["fetch", "btoa"]);
+    });
+
+    afterEach(() => {
+      sandbox.restore();
+    });
+
+    it("should convert username and password in URL to basic auth", async () => {
+      let parser = new $RefParser();
+
+      await parser.parse(
+        "https://username:password@petstore.swagger.io/v2/swagger.json"
+      );
+
+      expect(host.global.fetch).to.have.been.called.with(
+        "https://petstore.swagger.io/v2/swagger.json"
+      );
+
+      expect(host.global.btoa).to.have.been.called.with("username:password");
+    });
   });
 });
