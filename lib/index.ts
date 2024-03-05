48e4f0374e152618afd7b3b7c3a3aa839bf3d0a5
JonLuca DeCaro <jonluca.decaro@gmail.com>
2024-03-05 15:11:30 -0800

    fix(exports): export types

--- lib/index.ts
+++ lib/index.ts
@@ -19,17 +19,34 @@ import {
 import { ono } from "@jsdevtools/ono";
 import maybe from "./util/maybe.js";
 import type { ParserOptions } from "./options.js";
-import type { $RefsCallback, JSONSchema, SchemaCallback } from "./types/index.js";
+import type {
+  Plugin,
+  $RefsCallback,
+  JSONSchema,
+  SchemaCallback,
+  HTTPResolverOptions,
+  FileInfo,
+  ResolverOptions,
+  JSONSchemaObject,
+} from "./types/index.js";
 
-export { JSONParserError };
-export { InvalidPointerError };
-export { MissingPointerError };
-export { ResolverError };
-export { ParserError };
-export { UnmatchedParserError };
-export { UnmatchedResolverError };
+export {
+  JSONSchemaObject,
+  ResolverOptions,
+  ParserError,
+  UnmatchedResolverError,
+  ResolverError,
+  HTTPResolverOptions,
+  FileInfo,
+  UnmatchedParserError,
+  ParserOptions,
+  MissingPointerError,
+  InvalidPointerError,
+  JSONParserError,
+  Plugin,
+};
 
-type RefParserSchema = string | JSONSchema;
+export type RefParserSchema = string | JSONSchema;
 
 /**
  * This class parses a JSON schema, builds a map of its JSON references and their resolved values,
