dfbf4bb1c342c2cd49c41fc5761e28b0bb316539
Viacheslav Turovskyi <aeworxet@protonmail.com>
2025-07-07 06:31:17 +0300

    feat: add `onBundle` callback (#391)

---
+++ test/specs/bundle-callback/bundle-callback.spec.ts
@@ -0,0 +1,64 @@
+import { describe, it } from "vitest";
+import $RefParser from "../../../lib/index.js";
+import pathUtils from "../../utils/path.js";
+
+import { expect } from "vitest";
+import type { Options } from "../../../lib/options";
+
+describe("Schema with a $ref", () => {
+  it("should call onBundle", async () => {
+    const parser = new $RefParser();
+    const calls: any = [];
+    const schema = pathUtils.rel("test/specs/bundle-callback/bundle-callback.yaml");
+    const options = {
+      bundle: {
+        onBundle(path, value, parent, parentPropName) {
+          calls.push(JSON.parse(JSON.stringify({ path, value, parent, parentPropName })));
+        },
+      },
+    } as Options;
+    await parser.bundle(schema, options);
+
+    expect(calls).to.deep.equal([
+      {
+        path: "#/definitions/b",
+        value: { $ref: "#/definitions/b" },
+        parent: {
+          a: {
+            $ref: "#/definitions/b",
+          },
+          b: {
+            $ref: "#/definitions/a",
+          },
+        },
+        parentPropName: "a",
+      },
+      {
+        path: "#/definitions/a",
+        value: { $ref: "#/definitions/a" },
+        parent: {
+          a: {
+            $ref: "#/definitions/b",
+          },
+          b: {
+            $ref: "#/definitions/a",
+          },
+        },
+        parentPropName: "b",
+      },
+      {
+        path: "#/definitions/a",
+        value: { $ref: "#/definitions/a" },
+        parent: {
+          c: {
+            type: "string",
+          },
+          d: {
+            $ref: "#/definitions/a",
+          },
+        },
+        parentPropName: "d",
+      },
+    ]);
+  });
+});
